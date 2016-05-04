2b71d5886ccc9e189a6ca67e057c25cf1e64f675
Manu S Ajith <neo@codingarena.in>
2016-05-04 14:17:25 +0530

    Refactor code to be more moduler
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- lib/schizo.ex
+++ lib/schizo.ex
@@ -1,14 +1,22 @@
 defmodule Schizo do
 
   def uppercase(string) do
+    transformer(string, &upcaser/1)
+  end
+
+  def unvowel(string) do
+    string
+  end
+
+  defp transformer(string, transformation) do
     string
       |> String.split()
       |> Stream.with_index
-      |> Enum.map(&upcase_other_words/1)
+      |> Enum.map(transformation)
       |> Enum.join(" ")
   end
 
-  defp upcase_other_words({word, index}) do
+  defp upcaser({word, index}) do
     case {word, index} do
       {word, 0} -> word
       _ -> String.upcase(word)
