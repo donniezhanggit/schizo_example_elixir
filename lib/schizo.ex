d6c64b358e5919a851578ef1f21fe553b6c7e51c
Manu S Ajith <neo@codingarena.in>
2016-05-04 13:57:48 +0530

    Upcase all words in a collection except for the first; make tests for green
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- lib/schizo.ex
+++ lib/schizo.ex
@@ -1,2 +1,17 @@
 defmodule Schizo do
+
+  def uppercase(string) do
+    string
+      |> String.split()
+      |> Stream.with_index
+      |> Enum.map(&upcase_other_words/1)
+      |> Enum.join(" ")
+  end
+
+  def upcase_other_words({word, index}) do
+    case {word, index} do
+      {word, 0} -> word
+      _ -> String.upcase(word)
+    end
+  end
 end
