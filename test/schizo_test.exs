694407959b8ab265afaefdb34f1c7cb7c11666d1
Manu S Ajith <neo@codingarena.in>
2016-05-04 14:06:34 +0530

    Test for unvowel
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- test/schizo_test.exs
+++ test/schizo_test.exs
@@ -13,4 +13,16 @@ defmodule SchizoTest do
   test "uppercase converts every other word to uppercase" do
     assert(Schizo.uppercase("foo bar baz whee") == "foo BAR BAZ WHEE")
   end
+
+  test "unvowel doesnt change the first word" do
+    assert(Schizo.unvowel("foo") === "foo")
+  end
+
+  test "unvowel  removes the second words vowels" do
+    assert(Schizo.unvowel("foo bar") == "foo br")
+  end
+
+  test "unvowel removes every other words vowels" do
+    assert(Schizo.unvowel("foo bar baz whee") == "foo br bz wh")
+  end
 end
