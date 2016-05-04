766bb70d43cb725e83c1dbcbf297ba2e4b17452e
Manu S Ajith <neo@codingarena.in>
2016-05-04 13:57:16 +0530

    Add test for upcasing all words but first in a collection
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- test/schizo_test.exs
+++ test/schizo_test.exs
@@ -2,7 +2,15 @@ defmodule SchizoTest do
   use ExUnit.Case
   doctest Schizo
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "uppercase doesnt change the first word" do
+    assert(Schizo.uppercase("foo") === "foo")
+  end
+
+  test "uppercase converts the second word to uppercase" do
+    assert(Schizo.uppercase("foo bar") == "foo BAR")
+  end
+
+  test "uppercase converts every other word to uppercase" do
+    assert(Schizo.uppercase("foo bar baz whee") == "foo BAR BAZ WHEE")
   end
 end
